a50c05522ddc562ef9a8d5d7c665f88adead7469
Nael <willowitzy@gmail.com>
2024-04-29 02:52:29 -0300

    Botões

--- docs/js/code.js
+++ docs/js/code.js
@@ -3,13 +3,12 @@ function checkCode() {
     var code = prompt("Qual o código?");
 
     // Check if the code is correct
-    if (code === "??") {
+    if (code === "40") {
         // Redirect to the codex page
-        window.location.href = "./codex";
+        window.location.href = "/codex";
     } else {
         // Show an alert and redirect to the 404 error page
         alert("Código incorreto!");
         // window.location.href = "./404.html";
-        document.write("Tente de novo ou... retorne no dia 28/04/2024 às 16h00.");
     }
 }
--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(cors());
 // Serve static files located in the "public" folder using Express
 app.use(express.static('docs'));
 
+// Middleware to handle 404 errors
+app.use((req, res, next) => {
+  res.status(404).sendFile(__dirname + '/docs/404.html');
+});
+
 // Initialize server
 // Check: http://localhost:3000/
 const port = process.env.PORT || 3000;
